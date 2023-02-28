3e1bca34c3cb44a1bc54d2d10e45a75090cab730
jPabloGomez1913 <Juangomezramirez527@gmail.com>
2023-02-27 21:11:18 -0500

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello world!");
+        int a=3,b=4;
+        System.out.println(a+b);
+    }
+}
